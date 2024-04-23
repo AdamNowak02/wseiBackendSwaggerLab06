b4c729f31d5f44e0413ad9875d4c826cefdf3f46
AdamNowak02 <adam.nowak@microsoft.wsei.edu.pl>
2024-04-23 22:29:00 +0200

    Dodaj pliki projektów.

---
+++ Infrastructure/Migrations/20240423193943_migr.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class migr : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "290b01bd-2692-4fca-8c77-7ea76430195f");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "0f774d90-3931-43ff-9535-4c2fb0728d22");
+        }
+    }
+}
